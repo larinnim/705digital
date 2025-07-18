bb219d85cf8dd1691d18a9bdd3d67364aff14bea
Larinni Malheiros <larinnimalheiros@gmail.com>
2025-07-18 15:44:26 -0300

    update outDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://larinnim.github.io',
-  base: '/705digital',
+    site: 'https://larinnim.github.io',
+    outDir: './dist',
+    base: '/705digital',
 })
