09ddec9df7ebec4e7d87c99cc4b0772a68aa2e4c
Larinni Malheiros <larinnimalheiros@gmail.com>
2025-07-18 15:34:51 -0300

    added github changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://larinnim.github.io',
+  base: '/705digital',
+})
