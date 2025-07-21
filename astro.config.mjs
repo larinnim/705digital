b56af2e646d78b69e8f85a8bbe075903eba18609
Larinni Malheiros <larinnimalheiros@gmail.com>
2025-07-21 12:22:28 -0300

    change config again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://larinnim.github.io',
-    outDir: './dist',
+    base: '/705digital',
 })
 
