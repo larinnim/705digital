5d0e1030f50114abbe1a4dabc90b353693eaace2
Larinni Malheiros <larinnimalheiros@gmail.com>
2025-07-21 13:52:25 -0300

    trying to get site to run

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://larinnim.github.io',
-    base: '705digital',
+    base: '/705digital',
+    outDir: './dist',
 })
 
